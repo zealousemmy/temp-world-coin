7b2996ac222fdcf68e6989dc1886b36859c46f80
zeals <zealousemmy12@gmail.com>
2025-08-16 23:52:23 +0100

    updated suspense

--- src/app/(home)/sign-up/page.js
+++ src/app/(home)/sign-up/page.js
@@ -1,13 +1,17 @@
 "use client";
 import HomeWrapper from "@/components/HomeComponents/HomeWrapper";
 import SignupPageContents from "@/components/HomeComponents/SignupPageContents";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
-      <HomeWrapper hasNavFooter={false}>
-        <SignupPageContents />
-      </HomeWrapper>
+      <Suspense fallback={<div className="container py-12">Loading…</div>}>
+        <HomeWrapper hasNavFooter={false}>
+          <SignupPageContents />
+        </HomeWrapper>
+      </Suspense>
+
       {/* <SignupContents /> */}
     </>
   );
